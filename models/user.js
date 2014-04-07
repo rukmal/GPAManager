403489243236a429fb3da877e6855b6a38bcc797
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2014-04-06 22:14:22 -0700

    Fixed indentation in mongoose model

--- models/user.js
+++ models/user.js
@@ -11,63 +11,63 @@ var bcrypt = require('bcrypt');
 var SALT_WORK_FACTOR = 10;
 
 var userSchema = new mongoose.Schema({
-    firstName: {
-        type: String,
-        required: true
-    },
-    lastName: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    userName: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
+	firstName: {
+		type: String,
+		required: true
+	},
+	lastName: {
+		type: String,
+		required: true
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	userName: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	password: {
+		type: String,
+		required: true
+	}
 });
 
 userSchema.pre('save', function(next) {
-    var user = this;
+	var user = this;
 
-    // only hash the password if it has been modified (or is new)
-    if (!user.isModified('password')) return next();
+	// only hash the password if it has been modified (or is new)
+	if (!user.isModified('password')) return next();
 
-    // generate a salt
-    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-        if (err) return next(err);
+	// generate a salt
+	bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+		if (err) return next(err);
 
-        // hash the password using our new salt
-        bcrypt.hash(user.password, salt, function(err, hash) {
-            if (err) return next(err);
+		// hash the password using our new salt
+		bcrypt.hash(user.password, salt, function(err, hash) {
+			if (err) return next(err);
 
-            // override the cleartext password with the hashed one
-            user.password = hash;
-            next();
-        });
-    });
+			// override the cleartext password with the hashed one
+			user.password = hash;
+			next();
+		});
+	});
 });
 
 userSchema.methods.comparePasswords = function(candidatePassword) {
-    bcrypt.compare(candidatePassword, this.password, function(err, result) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(result);
-            console.log('middle');
-            return true;
-            // FIX THIS
-            // Try a double return
-        }
-    });
+	bcrypt.compare(candidatePassword, this.password, function(err, result) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log(result);
+			console.log('middle');
+			return true;
+			// FIX THIS
+			// Try a double return
+		}
+	});
 };
 
 module.exports = mongoose.model('User', userSchema);
